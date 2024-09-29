ce118f07b9fd5fefe0ec10de945acc6d2282520c
Bogdan Drozdowski <>
2024-09-29 23:13:58 +0200

    NTFS: remove support for ntfs_ files

--- src/wfs_ntfs.c
+++ src/wfs_ntfs.c
@@ -75,49 +75,33 @@
 
 #include "wipefreespace.h"
 
-#if ((defined HAVE_NTFS_NTFS_VOLUME_H) || (defined HAVE_NTFS_3G_NTFS_VOLUME_H)) \
+#if ((defined HAVE_NTFS_VOLUME_H) || (defined HAVE_NTFS_3G_VOLUME_H)) \
 	&& ((defined HAVE_LIBNTFS) || (defined HAVE_LIBNTFS_3G))
-# ifdef HAVE_NTFS_NTFS_VOLUME_H
-#  include <ntfs/ntfs_volume.h>
-#  include <ntfs/ntfs_version.h>
-#  include <ntfs/ntfs_attrib.h>		/* ntfs_attr_search_ctx() */
-#  include <ntfs/ntfs_mft.h>		/* ntfs_mft_records_write() */
-#  include <ntfs/ntfs_logfile.h>	/* ntfs_empty_logfile() */
+# ifdef HAVE_NTFS_VOLUME_H
+#  include <ntfs/volume.h>
+#  include <ntfs/version.h>
+#  include <ntfs/attrib.h>		/* ntfs_attr_search_ctx() */
+#  include <ntfs/mft.h>		/* ntfs_mft_records_write() */
+#  include <ntfs/logfile.h>		/* ntfs_empty_logfile() */
 # else
-#  include <ntfs-3g/ntfs_volume.h>
-#  include <ntfs-3g/ntfs_attrib.h>	/* ntfs_attr_search_ctx() */
-#  include <ntfs-3g/ntfs_mft.h>		/* ntfs_mft_records_write() */
-#  include <ntfs-3g/ntfs_logfile.h>	/* ntfs_empty_logfile() */
+#  include <ntfs-3g/volume.h>
+#  include <ntfs-3g/attrib.h>		/* ntfs_attr_search_ctx() */
+#  include <ntfs-3g/mft.h>		/* ntfs_mft_records_write() */
+#  include <ntfs-3g/logfile.h>		/* ntfs_empty_logfile() */
 # endif
 #else
-# if ((defined HAVE_NTFS_VOLUME_H) || (defined HAVE_NTFS_3G_VOLUME_H)) \
-	&& ((defined HAVE_LIBNTFS) || (defined HAVE_LIBNTFS_3G))
-#  ifdef HAVE_NTFS_VOLUME_H
-#   include <ntfs/volume.h>
-#   include <ntfs/version.h>
-#   include <ntfs/attrib.h>		/* ntfs_attr_search_ctx() */
-#   include <ntfs/mft.h>		/* ntfs_mft_records_write() */
-#   include <ntfs/logfile.h>		/* ntfs_empty_logfile() */
-#  else
-#   include <ntfs-3g/volume.h>
-#   include <ntfs-3g/attrib.h>		/* ntfs_attr_search_ctx() */
-#   include <ntfs-3g/mft.h>		/* ntfs_mft_records_write() */
-#   include <ntfs-3g/logfile.h>		/* ntfs_empty_logfile() */
+# if (defined HAVE_VOLUME_H) && ((defined HAVE_LIBNTFS) || (defined HAVE_LIBNTFS_3G))
+#  include <volume.h>
+#  ifndef HAVE_LIBNTFS_3G
+#   include <version.h>
 #  endif
+#  include <attrib.h>
+#  include <mft.h>
+#  include <logfile.h>
 # else
-#  if (defined HAVE_VOLUME_H) && ((defined HAVE_LIBNTFS) || (defined HAVE_LIBNTFS_3G))
-#   include <volume.h>
-#   ifndef HAVE_LIBNTFS_3G
-#    include <version.h>
-#   endif
-#   include <attrib.h>
-#   include <mft.h>
-#   include <logfile.h>
-#  else
-#   error Something wrong. NTFS requested, but headers or library missing.
+#  error Something wrong. NTFS requested, but headers or library missing.
 /* make a syntax error, because not all compilers treat #error as an error */
 Something wrong. NTFS requested, but headers or library missing.
-#  endif
 # endif
 #endif
 
@@ -163,21 +147,13 @@ struct filename
 	long long int	 size_alloc;	/* Allocated size (multiple of cluster size) */
 	long long int	 size_data;	/* Actual size of data */
 	long long int	 parent_mref;
-#if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	NTFS_FILE_ATTR_FLAGS	 flags;
-#else
 	FILE_ATTR_FLAGS	 flags;
-#endif
 	time_t		 date_c;	/* Time created */
 	time_t		 date_a;	/*	altered */
 	time_t		 date_m;	/*	mft record changed */
 	time_t		 date_r;	/*	read */
 	char		*name;		/* Filename in current locale */
-#if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	NTFS_FILE_NAME_TYPE_FLAGS name_space;
-#else
 	FILE_NAME_TYPE_FLAGS name_space;
-#endif
 	char		 padding[7];	/* Unused: padding to 64 bit. */
 };
 
@@ -194,11 +170,7 @@ struct data
 	long long int	 size_data;	/* Actual size of data */
 	long long int	 size_init;	/* Initialised size, may be less than data size */
 	long long int	 size_vcn;	/* Highest VCN in the data runs */
-#if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	ntfs_runlist_element *runlist;	/* Decoded data runs */
-#else
 	runlist_element *runlist;	/* Decoded data runs */
-#endif
 	int		 percent;	/* Amount potentially recoverable */
 	void		*data;		/* If resident, a pointer to the data */
 	char		 padding[4];	/* Unused: padding to 64 bit. */
@@ -216,11 +188,7 @@ struct ufile
 	long long int	 max_size;	/* Largest size we find */
 	int		 attr_list;	/* MFT record may be one of many */
 	int		 directory;	/* MFT record represents a directory */
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	NTFS_MFT_RECORD	*mft;		/* Raw MFT record */
-# else
 	MFT_RECORD	*mft;		/* Raw MFT record */
-# endif
 	char		 padding[4];	/* Unused: padding to 64 bit. */
 };
 
@@ -310,13 +278,8 @@ wipe_compressed_attribute (
 	unsigned char *mybuf = NULL;
 	s64 size, offset, ret = 0, wiped = 0;
 	u16 block_size;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	NTFS_VCN cur_vcn = 0;
-	ntfs_runlist *rlc;
-# else
 	VCN cur_vcn = 0;
 	runlist *rlc;
-# endif
 	s64 cu_mask;
 
 	size_t bufsize = 0;
@@ -356,11 +319,7 @@ wipe_compressed_attribute (
 		if ( ((cur_vcn & cu_mask) != 0) ||
 			(
 			 (((rlc + 1)->length) != 0) &&
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			 (rlc->lcn != NTFS_LCN_HOLE)
-# else
 			 (rlc->lcn != LCN_HOLE)
-# endif
 			)
 		   )
 		{
@@ -376,17 +335,9 @@ wipe_compressed_attribute (
 			return -1;
 		}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-		if (rlc->lcn == NTFS_LCN_HOLE)
-# else
 		if (rlc->lcn == LCN_HOLE)
-# endif
 		{
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			ntfs_runlist *rlt;
-# else
 			runlist *rlt;
-# endif
 
 			offset = cur_vcn - rlc->length;
 			if (offset == (offset & (~cu_mask)))
@@ -396,17 +347,10 @@ wipe_compressed_attribute (
 			}
 			offset = (offset & (~cu_mask)) << ntfs->cluster_size_bits;
 			rlt = rlc;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			while ((rlt - 1)->lcn == NTFS_LCN_HOLE)
-			{
-				rlt--;
-			}
-# else
 			while ((rlt - 1)->lcn == LCN_HOLE)
 			{
 				rlt--;
 			}
-# endif
 			while ( sig_recvd == 0 )
 			{
 				ret = ntfs_rl_pread (ntfs, na->rl, offset, two, &block_size);
@@ -844,11 +788,7 @@ utils_cluster_in_use (
 	   )
 	{
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-		attr = ntfs_attr_open (vol->lcnbmp_ni, NTFS_AT_DATA, NTFS_AT_UNNAMED, 0);
-# else
 		attr = ntfs_attr_open (vol->lcnbmp_ni, AT_DATA, AT_UNNAMED, 0);
-# endif
 		if ( (attr == NULL) || (sig_recvd != 0) )
 		{
 			return -1;
@@ -1043,11 +983,7 @@ destroy_record (
 # endif
 {
 	struct ufile *file = NULL;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	ntfs_runlist_element *rl = NULL;
-# else
 	runlist_element *rl = NULL;
-# endif
 	ntfs_attr *mft = NULL;
 	ntfs_attr_search_ctx *ctx = NULL;
 	wfs_errcode_t ret_wfs = WFS_SUCCESS;
@@ -1094,11 +1030,7 @@ destroy_record (
 	file->inode = record_no;
 
 	WFS_SET_ERRNO (0);
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	file->mft = (NTFS_MFT_RECORD *) malloc (ntfs->mft_record_size);
-# else
 	file->mft = (MFT_RECORD *) malloc (ntfs->mft_record_size);
-# endif
 	if ( file->mft == NULL )
 	{
 		error = WFS_GET_ERRNO_OR_DEFAULT (ENOMEM);
@@ -1110,11 +1042,7 @@ destroy_record (
 		return WFS_MALLOC;
 	}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	mft = ntfs_attr_open (ntfs->mft_ni, NTFS_AT_DATA, NTFS_AT_UNNAMED, 0);
-# else
 	mft = ntfs_attr_open (ntfs->mft_ni, AT_DATA, AT_UNNAMED, 0);
-# endif
 	if ( mft == NULL )
 	{
 		free_file (file);
@@ -1154,11 +1082,7 @@ destroy_record (
 	/* Wiping file names */
 	while ( sig_recvd == 0 )
 	{
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-        	if (ntfs_attr_lookup (NTFS_AT_FILE_NAME, NULL, 0, 0, 0LL, NULL, 0, ctx) != 0)
-# else
         	if (ntfs_attr_lookup (AT_FILE_NAME, NULL, 0, 0, 0LL, NULL, 0, ctx) != 0)
-# endif
         	{
 			break;	/* None / no more of that type */
 		}
@@ -1180,13 +1104,8 @@ destroy_record (
 		       		break;
 			}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-					1LL, ctx->mrec) != 0 )
-# else
 			if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 					1LL, ctx->mrec) != 0 )
-# endif
 			{
 				ret_wfs = WFS_BLKWR;
 				break;
@@ -1205,13 +1124,8 @@ destroy_record (
 			WFS_MEMSET (a_offset, 0, le32_to_cpu(ctx->attr->value_length));
 			if ( sig_recvd == 0 )
 			{
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-				if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-						1LL, ctx->mrec) != 0 )
-# else
 				if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 						1LL, ctx->mrec) != 0 )
-# endif
 				{
 					ret_wfs = WFS_BLKWR;
 					break;
@@ -1235,13 +1149,8 @@ destroy_record (
 		       		break;
 			}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-					1LL, ctx->mrec) != 0 )
-# else
 			if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 					1LL, ctx->mrec) != 0 )
-# endif
 			{
 				ret_wfs = WFS_BLKWR;
 				break;
@@ -1255,13 +1164,8 @@ destroy_record (
 			}
 		}
 		ctx->attr->value_length = 0;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-		if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-				1LL, ctx->mrec) != 0 )
-# else
 		if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 				1LL, ctx->mrec) != 0 )
-# endif
 		{
 			ret_wfs = WFS_BLKWR;
 			break;
@@ -1274,11 +1178,7 @@ destroy_record (
 	while ( sig_recvd == 0 )
 	{
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-        	if ( ntfs_attr_lookup (NTFS_AT_DATA, NULL, 0, 0, 0LL, NULL, 0, ctx) != 0 )
-# else
         	if ( ntfs_attr_lookup (AT_DATA, NULL, 0, 0, 0LL, NULL, 0, ctx) != 0 )
-# endif
         	{
 			break;	/* None / no more of that type */
 		}
@@ -1302,13 +1202,8 @@ destroy_record (
 			       		break;
 				}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-				if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-						1LL, ctx->mrec) != 0 )
-# else
 				if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 						1LL, ctx->mrec) != 0 )
-# endif
 				{
 					ret_wfs = WFS_BLKWR;
 					break;
@@ -1327,13 +1222,8 @@ destroy_record (
 				WFS_MEMSET (a_offset, 0, le32_to_cpu(ctx->attr->value_length));
 				if ( sig_recvd == 0 )
 				{
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-							1LL, ctx->mrec) != 0 )
-# else
 					if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 							1LL, ctx->mrec) != 0 )
-# endif
 					{
 						ret_wfs = WFS_BLKWR;
 						break;
@@ -1356,13 +1246,8 @@ destroy_record (
 			       		break;
 				}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-				if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-						1LL, ctx->mrec) != 0 )
-# else
 				if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 						1LL, ctx->mrec) != 0 )
-# endif
 				{
 					ret_wfs = WFS_BLKWR;
 					break;
@@ -1376,13 +1261,8 @@ destroy_record (
 				}
 			}
 			ctx->attr->value_length = 0;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-					1LL, ctx->mrec) != 0 )
-# else
 			if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 					1LL, ctx->mrec) != 0 )
-# endif
 			{
 				ret_wfs = WFS_BLKWR;
 				break;
@@ -1484,59 +1364,24 @@ destroy_record (
 			for ( pass = 0; (pass < wfs_fs.npasses) && (sig_recvd == 0); pass++ )
 			{
 				wfs_fill_buffer (pass, (unsigned char *) &(ctx->attr->lowest_vcn),
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					sizeof(NTFS_VCN),
-# else
-					sizeof(VCN),
-# endif
-					selected, wfs_fs);
+					sizeof(VCN), selected, wfs_fs);
 				wfs_fill_buffer (pass, (unsigned char *) &(ctx->attr->highest_vcn),
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					sizeof(NTFS_VCN),
-# else
-					sizeof(VCN),
-# endif
-					selected, wfs_fs);
+					sizeof(VCN), selected, wfs_fs);
 				wfs_fill_buffer (pass, (unsigned char *) &(ctx->attr->allocated_size),
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					sizeof(NTFS_VCN),
-# else
-					sizeof(VCN),
-# endif
-					selected, wfs_fs);
+					sizeof(VCN), selected, wfs_fs);
 				wfs_fill_buffer (pass, (unsigned char *) &(ctx->attr->data_size),
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					sizeof(NTFS_VCN),
-# else
-					sizeof(VCN),
-# endif
-					selected, wfs_fs);
+					sizeof(VCN), selected, wfs_fs);
 				wfs_fill_buffer (pass, (unsigned char *) &(ctx->attr->initialized_size),
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					sizeof(NTFS_VCN),
-# else
-					sizeof(VCN),
-# endif
-					selected, wfs_fs);
+					sizeof(VCN), selected, wfs_fs);
 				wfs_fill_buffer (pass, (unsigned char *) &(ctx->attr->compressed_size),
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-					sizeof(NTFS_VCN),
-# else
-					sizeof(VCN),
-# endif
-					selected, wfs_fs);
+					sizeof(VCN), selected, wfs_fs);
 				if ( sig_recvd != 0 )
 				{
 			       		break;
 				}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-				if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-						1LL, ctx->mrec) != 0 )
-# else
 				if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 						1LL, ctx->mrec) != 0 )
-# endif
 				{
 					ret_wfs = WFS_BLKWR;
 					break;
@@ -1555,13 +1400,8 @@ destroy_record (
 			ctx->attr->data_size = 0;
 			ctx->attr->initialized_size = 0;
 			ctx->attr->compressed_size = 0;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			if ( ntfs_mft_records_write (ntfs, NTFS_MK_MREF (record_no, 0),
-					1LL, ctx->mrec) != 0 )
-# else
 			if ( ntfs_mft_records_write (ntfs, MK_MREF (record_no, 0),
 					1LL, ctx->mrec) != 0 )
-# endif
 			{
 				ret_wfs = WFS_BLKWR;
 				free (rl);
@@ -1616,11 +1456,7 @@ wfs_ntfs_wipe_journal (
 	unsigned long int j;
 	int selected[WFS_NPAT] = {0};
 	unsigned char * buf = NULL;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	NTFS_MFT_REF log_ino;
-# else
 	MFT_REF log_ino;
-# endif
 	s64 blocksize;
 	unsigned int i;
 	unsigned int prev_percent = 50;
@@ -1645,11 +1481,7 @@ wfs_ntfs_wipe_journal (
 		return WFS_BADPARAM;
 	}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	log_ino = NTFS_FILE_LogFile;
-# else
 	log_ino = FILE_LogFile;
-# endif
 	ni = ntfs_inode_open (ntfs, log_ino);
 	if (ni == NULL)
 	{
@@ -1662,11 +1494,7 @@ wfs_ntfs_wipe_journal (
 	}
 	ntfs_inode_sync (ni);
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	na = ntfs_attr_open (ni, NTFS_AT_DATA, NTFS_AT_UNNAMED, 0);
-# else
 	na = ntfs_attr_open (ni, AT_DATA, AT_UNNAMED, 0);
-# endif
 	if (na == NULL)
 	{
 		ntfs_inode_close (ni);
@@ -1865,11 +1693,7 @@ wfs_ntfs_wipe_part (
 	char ** args_ntfswipe_copy = NULL;
 	child_id_t child_ntfswipe;
 	u64 nr_mft_records;
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	NTFS_MFT_REF inode_num;
-# else
 	MFT_REF inode_num;
-# endif
 	ntfs_inode *ni = NULL;
 	ntfs_attr *na = NULL;
 	unsigned char * buf;
@@ -2029,11 +1853,7 @@ wfs_ntfs_wipe_part (
 		/* wipe only if base MFT record */
 		if (ni->mrec->base_mft_record == 0)
 		{
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-			na = ntfs_attr_open (ni, NTFS_AT_DATA, NTFS_AT_UNNAMED, 0);
-# else
 			na = ntfs_attr_open (ni, AT_DATA, AT_UNNAMED, 0);
-# endif
 			if ( (na != NULL) && (sig_recvd==0) )
 			{
 				/* Only nonresident allowed. Resident ones are in the
@@ -2579,11 +2399,7 @@ wfs_ntfs_wipe_unrm (
 		return WFS_MALLOC;
 	}
 
-# if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	bitmapattr = ntfs_attr_open (ntfs->mft_ni, NTFS_AT_BITMAP, NTFS_AT_UNNAMED, 0);
-# else
 	bitmapattr = ntfs_attr_open (ntfs->mft_ni, AT_BITMAP, AT_UNNAMED, 0);
-# endif
 	if (bitmapattr == NULL)
 	{
 		free (buf);
@@ -2935,11 +2751,7 @@ wfs_ntfs_check_err (
 		return 1;
 	}
 	/* better than nothing... */
-#if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	return (ntfs->flags & NTFS_VOLUME_MODIFIED_BY_CHKDSK);
-#else
 	return (ntfs->flags & VOLUME_MODIFIED_BY_CHKDSK);
-#endif
 }
 
 /* ======================================================================== */
@@ -2971,27 +2783,15 @@ wfs_ntfs_is_dirty (
 	{
 		return 1;
 	}
-#if (defined HAVE_NTFS_NTFS_VOLUME_H)
-	if ( ((ntfs->flags & NTFS_VOLUME_IS_DIRTY) != 0)
-		|| ((ntfs->flags & NTFS_VOLUME_MODIFIED_BY_CHKDSK) != 0)
-# ifdef NVolWasDirty
-		|| (NVolWasDirty (ntfs) != 0)
-# endif
-		)
-	{
-		is_dirty = 1;
-	}
-#else
 	if ( ((ntfs->flags & VOLUME_IS_DIRTY) != 0)
 		|| ((ntfs->flags & VOLUME_MODIFIED_BY_CHKDSK) != 0)
-# ifdef NVolWasDirty
+#ifdef NVolWasDirty
 		|| (NVolWasDirty (ntfs) != 0)
-# endif
+#endif
 		)
 	{
 		is_dirty = 1;
 	}
-#endif
 	return is_dirty;
 }
 
